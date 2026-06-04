dde2fb11adbd641c12ed43e6870a7263153e7387
Jiulong Wang <jiulongw@gmail.com>
2026-06-03 21:27:13 -0700

    Fix build error in clang 21+

--- src/hunspell/hunspell.hxx
+++ src/hunspell/hunspell.hxx
@@ -197,7 +197,7 @@ class LIBHUNSPELL_DLL_EXPORTED Hunspell {
 
   int add(const std::string& word);
 
-  int add_with_flags(const std::string& word, const std::string& flags, const std::string& desc = NULL);
+  int add_with_flags(const std::string& word, const std::string& flags, const std::string& desc);
 
   /* add word to the run-time dictionary with affix flags of
    * the example (a dictionary word): Hunspell will recognize
